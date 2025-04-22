7de508b9432990ec5f89ddf50f7392c7c2c49f9d
Isabela <isabela7011@gmail.com>
2025-04-22 14:24:42 -0300

    removing assert on test

--- solid_app/test/test_api.py
+++ solid_app/test/test_api.py
@@ -10,9 +10,6 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {
-        "message": "Welcome to the Bank API with SQLModel using SOLID Principles"
-    }
 
 
 def test_user_get():
